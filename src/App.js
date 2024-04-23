e6124ffa16bb957da0dc4a178df40220d4740b75
yohaku7 <purple.cloud.88888@gmail.com>
2024-04-23 20:58:58 +0900

    不使用変数の更新（デプロイ用）

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import Form from './Form.js';
 
 function App() {
-  const [selectedMinorSubject, setSelectedMinorSubject] = useState("");
-
   return (
     <div className="App">
       <Form />
